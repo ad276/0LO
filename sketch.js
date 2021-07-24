9bc3699cbc810026bf623f6c580d99a337559a45
ad276 <85683804+ad276@users.noreply.github.com>
2021-07-24 05:03:22 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,25 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 
 let engine;
 let world;
-
-var ground;
-
-var top_wall;
 var ball;
+var ground;
+var con;
+var ball2;
+var con2;
 
-var btn1;
-var btn2;
+
 function setup() {
   createCanvas(400,400);
-
   engine = Engine.create();
-  world = engine.world;
   
-   var ball_options = {
-    restitution: 0.95,
+  world = engine.world;
+
+  var ball_options = {
+    restitution:0.95
   }
-   
   
   btn2 = createImg('up.png');
   btn2.position(20,30);
@@ -32,40 +28,58 @@ function setup() {
   
    
   
-  
-
-  ground =new Ground(200,390,400,20);
-
-
   ball = Bodies.circle(100,200,20,ball_options);
   World.add(world,ball);
-  
-  
-  
 
+  ball2 = Bodies.circle(350,10,12,ball_options);
+  World.add(world,ball2);
+  
+  con = Matter.Constraint.create({
+          pointA:{x:200,y:20},
+          bodyB:ball,
+          pointB:{x:0,y:0},
+          length:100,
+          stiffness:0.1
+        });
+  
+      World.add(world,con);
+      
+  //2nd constraint
+        
+   con2 = Matter.Constraint.create({
+          bodyA:ball,
+          pointA:{x:0,y:0},
+          bodyB:ball2,
+          pointB:{x:0,y:0},
+          length:100,
+          stiffness:0.1
+        });
+  
+      World.add(world,con2);
+  
   rectMode(CENTER);
   ellipseMode(RADIUS);
+  
 }
-
 
 function draw() 
 {
   background(51);
   Engine.update(engine);
-  
-  
+  ellipse(ball.position.x,ball.position.y,10);
+  ellipse(ball2.position.x,ball2.position.y,12);
 
-  ellipse(ball.position.x,ball.position.y,20);
-  ground.show();
+  push();
+  strokeWeight(2);
+  stroke(255);
+  line(con.pointA.x,con.pointA.y,ball.position.x,ball.position.y);
+  line(ball.position.x,ball.position.y,ball2.position.x,ball2.position.y);
+  pop();
   
-  Engine.update(engine);
 }
-
-
 function vForce()
 {
-  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
 }
   
-
 
